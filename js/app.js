6bb26a12b86811923101db3225f94deafe2bd5c8
adayc <adayc@hotmail.com>
2017-11-20 04:07:22 -0500

    Version 0.0.6 con JS comentado

--- js/app.js
+++ js/app.js
@@ -6,60 +6,49 @@ var taskcontents = document.getElementsByClassName('task-content');
 var linkTask = document.getElementById('add-task');
 var linkList = document.getElementById('add-list');
 var listTitle = document.getElementById('list-title');
-var btnTitle= document.getElementById('send-title');
-var toDo=document.getElementById('to-do');
+var btnTitle = document.getElementById('send-title');
+var toDo = document.getElementById('to-do');
 
 
 window.addEventListener('load', function(event) {
-// Desapareciendoel titulo de la lista
- listTitle.classList.add('not-see');
-  // Desapareciendo boton añadir tarea
-linkTask.classList.add('not-see');
-  // Desapareciendo los cuadros de tareas
-taskcontents[0].classList.add('not-see');//
- taskcontents[1].classList.add('not-see');
-// Desaparecer boton title
-btnTitle.classList.add('not-see')
+  // Ocultando titulo de la lista
+  listTitle.classList.add('not-see');
+  // Ocultando boton añadir tarea
+  linkTask.classList.add('not-see');
+  // Ocultando los cuadros de tareas
+  taskcontents[0].classList.add('not-see');//
+  taskcontents[1].classList.add('not-see');
+  // Ocultando boton title
+  btnTitle.classList.add('not-see');
 
-   /// agregar  el boton añadir lista linkTask.classList.add('see');
-
-   linkList.addEventListener('click', function(event) {
+  // Evento para añadir un titulo a la lista
+  linkList.addEventListener('click', function(event) {
     listTitle.classList.add('see');
     listTitle.classList.remove('not-see');
     btnTitle.classList.add('see');
     btnTitle.classList.remove('not-see');
     listTitle.focus();
     this.classList.add('not-see');
-    
-
   });
 
+  // Evento para guardar un titulo a la lista y crear otra adicional
 
   btnTitle.addEventListener('click', function(event) {
-    // Añadiendo tweet
-   // if (listTitle.innerHTML != '') {
-      cln=document.body.appendChild(toDo.cloneNode(true));
+    if (listTitle.innerHTML !== '') {
+      // Clonamos el nodo en el que se encuentra toda la lista de tareas
+      cln = document.body.appendChild(toDo.cloneNode(true));
       cln.children;
-      cln.children[0].children[1].innerHTML='';
+      cln.children[0].children[1].innerHTML = '';
       cln.children[0].children[1].focus();
-
-      
       listTitle.contentEditable = false;
       this.classList.add('not-see');
       this.classList.remove('see');
       linkTask.classList.add('see');
       linkTask.classList.remove('not-see');
-      
-      
-  //  }
+    }
   });
-
-
-
-
-
  
-
+  // Evento para añadir una tarea
   linkTask.addEventListener('click', function(event) {
     taskcontents[0].classList.remove('not-see');
     taskcontents[1].classList.remove('not-see');
@@ -68,19 +57,17 @@ btnTitle.classList.add('not-see')
     this.classList.add('not-see');
     this.classList.remove('see');
     task.focus();
-    
   });
 
-
-  // Añadir tareas 
+  // Evento para guardar tareas
   btnTask.addEventListener('click', function(event) {
-    // Añadiendo tweet
-    if (task.innerHTML != '') {
+    if (task.innerHTML !== '') {
+      // Creamos un nueevo 'div' para insertar al contenedor de tareas
       var newDiv = document.createElement('div');
       newDiv.innerHTML = task.innerHTML;
       newDiv.classList.add('task');
+      // Insrtamos la nueva tarea
       alltasks.appendChild(newDiv);
-
       task.innerHTML = '';
       task.focus();
     }
